8d0b4a07e41ed9571fe9940b4abd7df8e8993cdc
Anna Do <annado@gmail.com>
2014-02-26 23:24:10 -0800

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Anna Do on 2/26/14.
+//  Copyright (c) 2014 Anna Do. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
