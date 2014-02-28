a962d0356d8fd2811afa0bbff2c31f5d233f9e93
Anna Do <annado@gmail.com>
2014-02-27 22:29:54 -0800

    initial commit

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Anna Do on 2/26/14.
+//  Copyright (c) 2014 Anna Do. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
